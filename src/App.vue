e399eb883c8bb3b040a607a11d632f83ae07b6c1
Ramiiyan <Sriraguhan.2017380@iit.ac.lk>
2020-04-08 20:25:51 +0530

    start with movie portal

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+
+      <v-spacer></v-spacer>
+      
+      <v-toolbar-items >
+        <v-btn flat>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        <v-btn flat>
+          <router-link to="/about">About</router-link>
+        </v-btn>  
+      </v-toolbar-items>
+    
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
